130cc2a7ae2097b926bd820a68eb4ed296e15449
EddyManih <eddy.manih@polymtl.ca>
2024-11-09 15:24:44 -0500

    RCR Hand pose debug

---
+++ Assets/RCRManager.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class RCRManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text m_text;
+    bool m_RightHandPoseActiveActive, m_RightHandPoseActive;
+
+    bool m_HandPlacementRCRValid;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_RightHandPoseActiveActive = false;
+        m_RightHandPoseActive = false;
+        m_HandPlacementRCRValid = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_RightHandPoseActiveActive && !m_RightHandPoseActive)
+        {
+            m_HandPlacementRCRValid = false;
+        }
+    }
+
+
+    public void LeftHandRCRGesture(bool poseActive)
+    {
+        m_RightHandPoseActiveActive = poseActive;
+        m_text.text = m_RightHandPoseActiveActive ? "Left Hand Pose" : "";
+    }
+
+    public void RightHandRCRGesture(bool poseActive)
+    {
+        m_RightHandPoseActive = poseActive;
+        m_text.text = m_RightHandPoseActive ? "Right Hand Pose" : "";
+    }
+}
