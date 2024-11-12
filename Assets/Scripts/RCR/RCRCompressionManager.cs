915d542f2fc4faf55e43f9b91e2ca7e623b1a508
EddyManih <eddy.manih@polymtl.ca>
2024-11-12 12:43:15 -0500

    RCRCompressionManager first iteration

--- Assets/Scripts/RCR/RCRCompressionManager.cs
+++ Assets/Scripts/RCR/RCRCompressionManager.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class RCRCompressionManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text m_compressionStatus, m_compressionBPM;
+
+    bool m_compressionReachedValidDepth;
+    bool m_compressionTooDeep;
+
+    float m_timer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        m_compressionStarted = false;
+        m_compressionReachedValidDepth = false;
+        m_compressionTooDeep = false;
+        m_timer = 0.0f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        m_timer += Time.deltaTime;
+    }
+
+    public void CompressionStarted(bool compressionStatus) {
+        // Compression terminée
+        if (!compressionStatus) {
+            // Compression invalidée
+            if (m_compressionTooDeep) {
+                m_compressionStatus.text = "Compression too deep!";
+            } 
+            // Compression valide
+            else if (m_compressionReachedValidDepth) {
+                m_compressionStatus.text = "Good compression!";
+            }
+            // Compression pas assez profonde
+            else {
+                m_compressionStatus.text = "Compression too shallow";
+            }
+        }
+
+        // Nouvelle compression commencée
+        if (compressionStatus) {
+            m_compressionReachedValidDepth = false;
+            m_compressionTooDeep = false;
+        }
+    }
+
+    public void CompressionReachedValidDepth() {
+        m_compressionReachedValidDepth = true;
+    }
+
+    public void compressionTooDeep() {
+        m_compressionTooDeep = true;
     }
 }
