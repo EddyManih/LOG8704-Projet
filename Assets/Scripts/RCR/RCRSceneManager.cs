30443515eed3148381a24ef3daadefd72abdccad
MissChungus <telesanti58@hotmail.com>
2024-11-16 12:34:30 -0500

    added script to put scene elements in front of you in rcr scene

---
+++ Assets/Scripts/RCR/RCRSceneManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RCRSceneManager : MonoBehaviour
+{
+    [SerializeField] Transform head;
+    private bool firstFrame = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstFrame) {
+            this.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * 0.6f;
+            this.transform.LookAt(new Vector3(head.position.x, this.transform.position.y, head.position.z));
+            this.transform.forward *= -1;
+            this.transform.Rotate(0, 90 ,0);
+            firstFrame = false;
+        }
+    }
+}
