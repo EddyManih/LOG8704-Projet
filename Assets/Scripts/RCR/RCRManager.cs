a5ca684f91ddfc5811beb2a7e68e96fabb861d80
MissChungus <telesanti58@hotmail.com>
2024-11-16 16:26:31 -0500

    modified rcr manager

--- Assets/Scripts/RCR/RCRManager.cs
+++ Assets/Scripts/RCR/RCRManager.cs
@@ -50,13 +50,22 @@ public class RCRManager : MonoBehaviour
             case RCRState.DetectHandPoseOnChest:
                 if (RCRGestureManager.Instance.HandPoseOnChestValid()) {
                     m_handPlacementValidText.text = "Hand placement: Valid";
-                    SwitchState(RCRState.DetectHandsGesture, "DetectHandsGesture");
-                    return;
+                    //SwitchState(RCRState.Compressions, "Compressions");
+                    m_state = RCRState.DetectHandsGesture;
                 }
                 m_handPlacementValidText.text = "Hand placement: Invalid";
                 break;
 
             case RCRState.DetectHandsGesture:
+                if (RCRGestureManager.Instance.readyToStartCompressions()) {
+                    SwitchState(RCRState.Compressions, "Compressions");
+                }
+                else if (!RCRGestureManager.Instance.handOnChest) {
+                    m_RCRStateText.text = "Please keep your hand on the chest";
+                }
+                else if (!RCRGestureManager.Instance.handOnHand) {
+                    m_RCRStateText.text = "Please place your other hand on top of your favored hand";
+                }
                 break;
 
             case RCRState.Compressions:
