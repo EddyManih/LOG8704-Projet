5c3b2ca4f7523ddc670640c2f2b3dcbdf44ab044
EddyManih <eddy.manih@polymtl.ca>
2024-11-16 16:16:58 -0500

    [Feat] Chest moves with compressions

--- Assets/Scripts/RCR/Compressions/RCRCompressionManager.cs
+++ Assets/Scripts/RCR/Compressions/RCRCompressionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,12 +7,16 @@ public class RCRCompressionManager : MonoBehaviour
 {
     [SerializeField] TMP_Text m_compressionStatusText, m_compressionBPMText;
 
-    bool m_compressionReachedValidDepth;
-    bool m_compressionTooDeep;
+    bool m_compressionReachedValidDepth, m_compressionTooDeep, m_ongoingCompression;
 
     float m_timer;
     List<float> m_compressionTimes;
     int m_compressionBPM;
+
+    [SerializeField] Transform m_handTransform, m_chestTransform;
+    [SerializeField] float m_maxChestOffset = 0.01f;
+    float m_handVerticalPos;
+    Vector3 m_chestInitialPos;
 
     public static RCRCompressionManager Instance {get; private set;}
 
@@ -32,15 +37,22 @@ public class RCRCompressionManager : MonoBehaviour
         m_timer = 0.0f;
         m_compressionTimes = new List<float>();
         m_compressionBPM = 0;
+        m_handVerticalPos = 0.0f;
+        m_chestInitialPos = m_chestTransform.position;
+
     }
 
     void Update() {
         m_timer += Time.deltaTime;
+
+        HandleChestMovement();
     }
 
     public void CompressionStarted(bool compressionStatus) {
+        m_ongoingCompression = compressionStatus;
+
         // Compression terminée
-        if (!compressionStatus) {
+        if (!m_ongoingCompression) {
             m_compressionTimes.Add(m_timer);
             // Compression invalidée
             if (m_compressionTooDeep) {
@@ -57,7 +69,9 @@ public class RCRCompressionManager : MonoBehaviour
         }
 
         // Nouvelle compression commencée
-        if (compressionStatus) {
+        if (m_ongoingCompression) {
+            m_handVerticalPos = m_handTransform.position.y;
+
             if (m_compressionTimes.Count == 2) {
                 // 1 compression/deltaT s * 60s/min => compression/min
                 m_compressionBPM = (int) (60.0f / (m_compressionTimes[1] - m_compressionTimes[0]));
@@ -79,5 +93,21 @@ public class RCRCompressionManager : MonoBehaviour
 
     public void compressionTooDeep() {
         m_compressionTooDeep = true;
+    }
+
+    private void HandleChestMovement() {
+        if (m_ongoingCompression) {
+            float handOffset = Math.Abs(m_handVerticalPos - m_handTransform.position.y);
+
+            m_chestTransform.transform.position = new Vector3(
+                m_chestInitialPos.x,
+                Mathf.Max(m_chestTransform.position.y - handOffset, m_chestInitialPos.y - m_maxChestOffset),
+                m_chestInitialPos.z
+            );
+        } else {
+            m_chestTransform.position = m_chestInitialPos;
+        }
+
+        m_handVerticalPos = m_handTransform.position.y;
     }
 }
