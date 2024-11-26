166193b20a36ce6db068c095f86636122234b85c
EddyManih <eddy.manih@polymtl.ca>
2024-11-26 13:37:20 -0500

    [Feat] Added sound when checking breathing

--- Assets/Scripts/BreathingManager.cs
+++ Assets/Scripts/BreathingManager.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class BreathingManager : MonoBehaviour
 {
-    [SerializeField] float m_CheckBeathingTimerThreshold = 3.0f;
+    [SerializeField] float m_CheckBreathingTimerThreshold = 3.0f;
+    [SerializeField] AudioClip m_beepClip;
+
+
     bool m_checkedBreathing, m_checkingAbdomen, m_checkingHead; 
     float m_timer;
+    float m_playSoundInterval;
+    AudioSource m_audioSource;
+
     public static BreathingManager Instance {get; private set;}
 
     void Awake() {
@@ -18,10 +25,13 @@ public class BreathingManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioSource.clip = m_beepClip;
         m_checkedBreathing = false;
         m_checkingAbdomen = false;
         m_checkingHead = false;
         m_timer = 0.0f;
+        m_playSoundInterval = 1.0f;
     }
 
     // Update is called once per frame
@@ -30,8 +40,16 @@ public class BreathingManager : MonoBehaviour
         if (m_checkingAbdomen && m_checkingHead) {
             m_timer += Time.deltaTime;
 
-            if (m_timer > m_CheckBeathingTimerThreshold) {
-                m_checkedBreathing = true;
+            if (m_timer > m_playSoundInterval && m_playSoundInterval != m_CheckBreathingTimerThreshold) {
+                m_audioSource.Play(0);
+                m_playSoundInterval++;
+            }
+
+            if (m_timer > m_CheckBreathingTimerThreshold) {
+                m_audioSource.pitch = 0.5f;
+                m_audioSource.Play(0);
+
+                StartCoroutine(WaitForAudio());
             }
         }
         else {
@@ -49,5 +67,15 @@ public class BreathingManager : MonoBehaviour
 
     public void CheckingHead(bool status) {
         m_checkingHead = status;
+    }
+
+    private IEnumerator WaitForAudio()
+    {
+        while (m_audioSource.isPlaying)
+        {
+            yield return null;
+        }
+
+        m_checkedBreathing = true;
     }
 }
